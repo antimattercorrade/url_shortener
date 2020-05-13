0c74a67baae5dfa0f1cb1f87d60a533f550219cf
antimattercorrade <kush991013@gmail.com>
2020-05-13 20:03:41 +0530

    first commit

---
+++ shortener/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
+from .models import KirrURL
+from .forms import SubmitUrlForm
+from analytics.models import ClickEvent
+
+# Create your views here.
+
+
+
+
+# def kirr_redirect_view(request,shortcode=None,*args,**kwargs):
+	
+# 	# try:
+# 	# 	obj = KirrURL.objects.get(shortcode=shortcode)
+# 	# except:
+# 	# 	obj = KirrURL.objects.all().first()
+
+
+# 	obj = get_object_or_404(KirrURL,shortcode=shortcode)
+# 	# obj_url=obj.url
+
+
+# 	# obj_url=None
+# 	# qs= KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
+# 	# if qs.exists() and qs.count()==1:
+# 	# 	obj=qs.first()
+# 	# 	obj_url=obj.url
+# 	return HttpResponseRedirect(obj.url)
+
+def home_view_fbv(request,*args,**kwargs):
+	if request.method == "POST":
+		print(request.POST)
+	return render(request,"shortener/home.html",{})
+
+
+class HomeView(View):
+	def get(self,request,*args,**kwargs):
+		the_form = SubmitUrlForm()
+		context ={
+			"title": "Submit Url 123",
+			"form" : the_form
+		}
+		return render(request,"shortener/home.html",context)
+
+	def post(self,request,*args,**kwargs):
+		form = SubmitUrlForm(request.POST)
+		context ={
+			"title": "Submit Url 123",
+			"form" : form
+		}
+		template= "shortener/home.html"
+		if form.is_valid():
+			new_url = form.cleaned_data.get("url")
+			obj, created = KirrURL.objects.get_or_create(url=new_url)
+			context = {
+				"object": obj,
+				"created": created,
+			}	
+			if created:
+				template = "shortener/success.html"
+			else:
+				template = "shortener/already-exists.html"
+		
+		return render(request,template,context)
+
+
+
+class URLRedirectView(View):
+	def get(self,request,shortcode=None,*args,**kwargs):
+		qs = KirrURL.objects.filter(shortcode__iexact=shortcode)
+		if qs.count()!= 1 and not qs.exists():
+			raise Http404
+		obj = qs.first()
+		print(ClickEvent.objects.create_event(obj))
+		return HttpResponseRedirect(obj.url)
+
+
+		# obj = get_object_or_404(KirrURL,shortcode=shortcode)
+		# print(ClickEvent.objects.create_event(obj))
+		# return HttpResponseRedirect(obj.url)
