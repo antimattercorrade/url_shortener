732c95ee37ba7d3755585390433823a52900ee22
antimattercorrade <kush991013@gmail.com>
2020-05-14 19:23:47 +0530

    thirteenth commit

--- short_en/hostsconf/views.py
+++ short_en/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
 
-DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL","https://new-shortener.herokuapp.com")
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL","https://shortnewurl.co")
 
 def wildcard_redirect(request,path=None):
 	new_url=DEFAULT_REDIRECT_URL
--- short_en/settings/production.py
+++ short_en/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = '==yur&fmmb#_fs)yi=z8!-dl&-)+*ngyq-+%g+h8bzisu3+@_1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['new-shortener.herokuapp.com']
+ALLOWED_HOSTS = ['shortnewurl.co', 'new-shortener.herokuapp.com']
 
 
 # Application definition
@@ -62,8 +62,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'short_en.urls'
 ROOT_HOSTCONF = 'short_en.hosts'
 DEFAULT_HOST = 'www'
-DEFAULT_REDIRECT_URL = 'https://new-shortener.herokuapp.com'
-PARENT_HOST = "new-shortener.herokuapp.com"
+DEFAULT_REDIRECT_URL = 'https://shortnewurl.co'
+PARENT_HOST = "shortnewurl.co"
 
 TEMPLATES = [
     {
